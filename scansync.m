d1a4cb8bdd256f0d79ffa396a3a4ca098e979bfb
Johan Carlin <johan.carlin@gmail.com>
2020-09-22 15:48:30 +0100

    use undocumented NI API

--- scansync.m
+++ scansync.m
@@ -1,4 +1,4 @@
-function [resptime,respnumber,daqret] = scansync(ind,waituntil,tmstrigger)
+function [resptime,respnumber,daqret] = scansync(ind,waituntil)
 % synchronise with volume acquisition trigger pulses and record button 
 % presses from CBU National Instruments MRI scanner interface.
 %
@@ -50,16 +50,16 @@ function [resptime,respnumber,daqret] = scansync(ind,waituntil,tmstrigger)
 %
 % % wait 2s no matter what (but keep track of all responses)
 % [~, ~, daqstate] = scansync([],GetSecs+2);
-% % time stamp for last recording trigger, which may have occurred during
-% % the 2s interval in the example above.
+% % time stamp for last scanner pulse, which may have occurred during % the 2s interval
+% in the example above.
 % lastpulse_time = daqstate.lastresp(1);
 %
 % 2017-04-13 J Carlin, MRC CBU.
 % 2019-06-19 Added support for two-handed mode
 % 2019-10-02 Documentation, respnumber is scalar return
-% 2020-09-21 Switch to undocumented NI API for performance, trigger send for TMS fMRI
+% 2020-09-21 Switch to undocumented NI API for performance
 %
-% [resptime,respnumber,daqstate] = scansync(ind,waituntil,tmstrigger)
+% [resptime,respnumber,daqstate] = scansync(ind,waituntil)
 
 persistent daqstate
 
@@ -72,71 +72,86 @@ if ~exist('waituntil','var') || isempty(waituntil) || isnan(waituntil)
 end
 assert(~isinf(waituntil) || ~isempty(ind), ...
     'unspecified channel index must be combined with finite waituntil duration')
-if ~exist('tmstrigger','var') || isempty(tmstrigger)
-    tmstrigger = false;
-end
 
-if isstr(ind) && strcmpi(ind,'reset')
+if ischar(ind) && strcmpi(ind,'reset')
     % don't handle conflicting inputs
-    assert(~tmstrigger, 'tmstrigger argument unsupported in reset mode');
     assert(waituntil~=0, 'must set tr as second arg in reset mode');
     % special case to handle re-initialising sync
     if ~isempty(daqstate)
+        % TODO - check that this works with new undocumented API
         daqstate.hand.release();
     end
     daqstate = [];
     ind = [];
-    tmstrigger = false;
 end
+
+% constants
+% input channels in order scanner pulse, buttonbox 1, buttonbox 2
+channels = {...
+    '/dev1/port0/line0', ...
+    '/dev1/port0/line1', ...
+    '/dev1/port0/line2', ...
+    '/dev1/port0/line3', ...
+    '/dev1/port0/line4', ...
+    '/dev1/port0/line5', ...
+    '/dev1/port0/line6', ...
+    '/dev1/port0/line7', ...
+    '/dev1/port1/line0'};
 
 if isempty(daqstate)
     % special initialisation mode
+    fprintf('initialising...\n');
     tr = waituntil;
     % ordinarily infinite wait durations are fine, but not if you're
     % initialising a new session
     assert(~isinf(tr) && isscalar(tr),'tr must be finite, numeric, scalar');
     % check for DAQ
-    hasdaq = false;
-    try
-        D = daq.getDevices;
-        hasdaq = ~isempty(D) && D.isvalid && any(strcmp({D.Vendor.ID},'ni')) && ...
-                D.Vendor.isvalid && D.Vendor.IsOperational;
-    catch err
-        if ~strcmp(err.identifier,'MATLAB:undefinedVarOrClass')
-            rethrow(err);
-        end
-    end
-    if hasdaq
-        fprintf('initialising new scanner card connection\n');
-        warning off daq:Session:onDemandOnlyChannelsAdded
-        daqstate.hand = daq.createSession('ni');
-        daqstate.tr = tr;
-        % Add channels for scanner pulse
-        daqstate.hand.addDigitalChannel('Dev1', 'port0/line0', 'InputOnly');
-        % Add channels for button 1-4
-        daqstate.hand.addDigitalChannel('Dev1', 'port0/line1', 'InputOnly');
-        daqstate.hand.addDigitalChannel('Dev1', 'port0/line2', 'InputOnly');
-        daqstate.hand.addDigitalChannel('Dev1', 'port0/line3', 'InputOnly');
-        daqstate.hand.addDigitalChannel('Dev1', 'port0/line4', 'InputOnly');
-        % Add channels for button 5-8
-        daqstate.hand.addDigitalChannel('Dev1', 'port0/line5', 'InputOnly');
-        daqstate.hand.addDigitalChannel('Dev1', 'port0/line6', 'InputOnly');
-        daqstate.hand.addDigitalChannel('Dev1', 'port0/line7', 'InputOnly');
-        daqstate.hand.addDigitalChannel('Dev1', 'port1/line0', 'InputOnly');
+    daqstate.tr = tr;
+    daqstate.nchannel = numel(channels);
+    if hasdaq()
+        fprintf('initialising new scanner card receive connection\n');
         daqstate.emulate = false;
-        daqstate.nchannel = numel(daqstate.hand.Channels);
+        warning off daq:Session:onDemandOnlyChannelsAdded
+        % from NI_DAQmxCreateTask
+        [status, daqstate.hand] = daq.ni.NIDAQmx.DAQmxCreateTask(char(0), uint64(0));
+        daq.ni.utility.throwOrWarnOnStatus(status);
+        % /from NI_DAQmxCreateTask
+        % Add channels
+        for this_in = channels
+            this_in_str = this_in{1};
+            % from NI_DAQmxCreateDIChan
+            status = daq.ni.NIDAQmx.DAQmxCreateDIChan(daqstate.hand, ...
+                this_in_str, char(0), daq.ni.NIDAQmx.DAQmx_Val_ChanForAllLines);
+            daq.ni.utility.throwOrWarnOnStatus(status);
+            % /from NI_DAQmxCreateDIChan
+        end
+        % from NI_DAQmxStartTask
+        status = daq.ni.NIDAQmx.DAQmxStartTask(daqstate.hand);
+        daq.ni.utility.throwOrWarnOnStatus(status);
+        % /from NI_DAQmxStartTask
+        daqstate.checkfun = @inputSingleScan_lowlevel;
     else
         fprintf(['NI CARD NOT AVAILABLE - entering emulation mode with tr=' ...
             mat2str(tr) '\n']);
         fprintf('if you see this message in the scanner, DO NOT PROCEED\n')
         % struct with a function handle in place of inputSingleScan
-        daqstate = daqemulator(tr);
+        daqstate.emulate = true;
+        % dummy 
+        daqstate.hand.release = @(x)fprintf('reset scansync session.\n');
+        daqstate.emulatekeys = [KbName('v'), KbName('b'), KbName('n'), KbName('m'), ...
+            KbName('f'), KbName('d'), KbName('s'), KbName('a')];
+        daqstate.firstcall = true;
+        daqstate.checkfun = @inputSingleScan_emulate;
     end
+    % time stamps for the first observed response at each channel
     daqstate.firstresp = NaN([1,daqstate.nchannel]);
+    % time stamps for the last *valid* response at each channel
     daqstate.lastresp = NaN([1,daqstate.nchannel]);
+    % time stamps for the current response, if valid
+    % (why both lastresp and thisresp? To avoid double counting responses)
     daqstate.thisresp = NaN([1,daqstate.nchannel]);
     daqstate.nrecorded = zeros(1,daqstate.nchannel);
-    % we count pulses if they are >.02s apart, and button presses if they are
+    % we count pulses if they are >.006s apart, and button presses if they are
     % more than .2s apart
     daqstate.pulsedur = [.006,ones(1,daqstate.nchannel-1)*.2];
 end
@@ -144,9 +159,9 @@ end
 % always call once (so we get an update even if waituntil==0)
 daqstate = checkdaq(daqstate);
 while (GetSecs < waituntil) && all(isnan(daqstate.thisresp(ind)))
-    daqstate = checkdaq(daqstate);
     % avoid choking the CPU, but don't wait so long that we might miss a pulse
-    WaitSecs(min(daqstate.pulsedur)/3);
+    WaitSecs(0.001);
+    daqstate = checkdaq(daqstate);
 end
 
 % so now this will be NaN if no responses happened, or otherwise not nan. Note
@@ -167,14 +182,20 @@ if nargout > 2
 end
 
 function daqstate = checkdaq(daqstate)
+
+% time stamp of the check, before any other overhead
 timenow = GetSecs;
-% call the DAQ - trigger, buttons 1:9
-daqflags = ~daqstate.hand.inputSingleScan();
+
+% perilously close to OO here
+[daqflags, daqstate] = feval(daqstate.checkfun, daqstate);
+% inverted coding
+daqflags = ~daqflags;
 
 % wipe whatever we had in thisresp from the last call
 daqstate.thisresp = NaN([1,daqstate.nchannel]);
 
-% if any of the responses are new, keep track of when this occurred
+% if this is the first time we observe any of the channels, we need to log the time
+% stamp of this into all registers.
 newresp = isnan(daqstate.firstresp);
 daqstate.firstresp(daqflags & newresp) = timenow;
 daqstate.lastresp(daqflags & newresp) = timenow;
@@ -190,32 +211,14 @@ if any(valid)
     daqstate.nrecorded(valid) = daqstate.nrecorded(valid)+1;
 end
 
-function daqstate = daqemulator(tr)
-
-daqstate.tr = tr;
-daqstate.emulate = true;
-% fake method
-daqstate.hand.inputSingleScan = @emulatecard;
-% dummy 
-daqstate.hand.release = @(x)fprintf('reset scansync session.\n');
-daqstate.emulatekeys = [KbName('v'), KbName('b'), KbName('n'), KbName('m'), ...
-    KbName('f'), KbName('d'), KbName('s'), KbName('a')];
-daqstate.firstcall = true;
-daqstate.nchannel = 9;
-
-function flags = emulatecard()
-
-% pull in the current daqstate
-daqstate = evalin('caller','daqstate');
+function [flags, daqstate] = inputSingleScan_emulate(daqstate)
 
 % NB inverted coding on NI cards
 flags = true(1,daqstate.nchannel);
 if daqstate.firstcall
-    % make sure we return nothing the very first time we call (on init). This is
-    % hacky but important to avoid starting the pulse emulator too early.
+    % make sure we return nothing the very first time we call (on reset). This is
+    % important to avoid starting the pulse emulator too early.
     daqstate.firstcall = false;
-    % write it back to the caller
-    assignin('caller','daqstate',daqstate);
     return
 end
 
@@ -239,3 +242,13 @@ if keyisdown
     % need to offset by 1 to stay clear of pulse channel
     flags(ind+1) = false;
 end
+
+function [flags, daqstate] = inputSingleScan_lowlevel(daqstate)
+
+% adapted from NI_DAQmxReadDigitalLines
+[status,flags,~] = daq.ni.NIDAQmx.DAQmxReadDigitalLines(daqstate.hand, ...
+    int32(1),double(10),uint32(daq.ni.NIDAQmx.DAQmx_Val_GroupByChannel), ...
+    uint8(zeros(1,daqstate.nchannel)),uint32(daqstate.nchannel), ...
+    int32(0),int32(0),uint32(0));
+daq.ni.utility.throwOrWarnOnStatus(status);
+% /adapted from NI_DAQmxReadDigitalLines
