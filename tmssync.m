1c883fd2e2a80807e5d14993540e2e4b3fce63ad
Johan Carlin <johan.carlin@gmail.com>
2020-09-22 21:31:11 +0100

    working version for testing

--- tmssync.m
+++ tmssync.m
@@ -2,7 +2,11 @@ function tmssync(ind)
 
 persistent daqstate
 
-if isstr(ind) && strcmpi(ind,'reset')
+if ~exist('ind','var')
+    ind = [];
+end
+
+if ischar(ind) && strcmpi(ind,'reset')
     % special case to handle re-initialising sync
     if ~isempty(daqstate)
         % TODO - check that this works with new undocumented API
@@ -12,20 +16,21 @@ if isstr(ind) && strcmpi(ind,'reset')
     ind = [];
 end
 
-% constants
-channels = {'/dev1/port2/line7'};
 
 if isempty(daqstate)
-    if hasdaq
+    % special initialisation mode
+    fprintf('initialising...\n');
+    daqstate.channels = {'/dev1/port2/line7'};
+    daqstate.nchannel = numel(daqstate.channels);
+    if hasdaq()
         fprintf('initialising new scanner card transmit connection\n');
-        daqstate.nchannel = numel(channels);
         daqstate.emulate = false;
         warning off daq:Session:onDemandOnlyChannelsAdded
         % from NI_DAQmxCreateTask
         [status, daqstate.hand] = daq.ni.NIDAQmx.DAQmxCreateTask(char(0), uint64(0));
         daq.ni.utility.throwOrWarnOnStatus(status);
         % /from NI_DAQmxCreateTask
-        for this_out = channels
+        for this_out = daqstate.channels
             this_out_str = this_out{1};
             % from NI_DAQmxCreateDOChan
             status = daq.ni.NIDAQmx.DAQmxCreateDOChan(daqstate.hand, ...
@@ -33,12 +38,41 @@ if isempty(daqstate)
             daq.ni.utility.throwOrWarnOnStatus(status);
             % /from NI_DAQmxCreateDOChan
         end
+        % from NI_DAQmxStartTask
         status = daq.ni.NIDAQmx.DAQmxStartTask(daqstate.hand);
         daq.ni.utility.throwOrWarnOnStatus(status);
+        % /from NI_DAQmxStartTask
+        daqstate.sendfun = @sendtrigger_lowlevel;
     else
-        fprintf(['NI CARD NOT AVAILABLE - entering emulation mode with tr=' ...
-            mat2str(tr) '\n']);
+        fprintf('NI CARD NOT AVAILABLE - entering trigger emulation mode\n')
         fprintf('if you see this message in the scanner, DO NOT PROCEED\n')
-        % struct with a function handle in place of inputSingleScan
-        daqstate = daqemulator(tr);
+        daqstate.emulate = true;
+        % dummy 
+        daqstate.hand.release = @(x)fprintf('reset scansync session.\n');
+        daqstate.sendfun = @sendtrigger_emulate;
     end
+    % all setup done
+    return
+end
+
+% if we make it here, it's trigger time
+% recode indices to column vector
+triggers = zeros([daqstate.nchannel, 1], 'uint8');
+% matlab is fun
+assert(all(ind <= daqstate.nchannel), 'index out of range');
+triggers(ind) = 1;
+daqstate.sendfun(daqstate, triggers);
+
+function sendtrigger_lowlevel(daqstate, triggers)
+
+% from NI_DAQmxWriteDigitalLines
+[status, ~] = daq.ni.NI_DAQmxWriteDigitalLines(daqstate.hand, ...
+    int32(1), uint32(false), double(10), ...
+    uint32(daq.ni.NIDAQmx.DAQmx_Val_GroupByChannel), ...
+    triggers, int32(0), uint32(0));
+daq.ni.utility.throwOrWarnOnStatus(status);
+% /from NI_DAQmxWriteDigitalLines
+
+function sendtrigger_emulate(daqstate, triggers)
+
+% empty for now
